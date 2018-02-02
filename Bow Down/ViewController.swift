23f6b956d065142bf54f4a989ce505eccb0b3e33
Austin Quach <austinquachh@gmail.com>
2018-02-01 20:03:32 -0800

    initial push

--- Bow Down/ViewController.swift
+++ Bow Down/ViewController.swift
@@ -7,19 +7,42 @@
 //
 
 import UIKit
+import AVFoundation
 
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        button.backgroundColor = .clear
+        button.layer.cornerRadius = 8
+        button.layer.borderWidth = 1
+        button.layer.borderColor = UIColor.black.cgColor
+        button.contentEdgeInsets = UIEdgeInsetsMake(5,5,5,5) 
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
+    
+    @IBOutlet weak var button: UIButton!
+    var _sfx: AVAudioPlayer?
+    var audioPlayer = AVAudioPlayer()
+    
+    @IBAction func button(_ sender: Any) {
+        do {
+            try playSound()
+        } catch {
+            print(error)
+        }
+    }
+    
+    func playSound() throws {
+        let url = Bundle.main.url(forResource: "bow", withExtension: "mp3")!
+        audioPlayer = try AVAudioPlayer(contentsOf: url)
+        audioPlayer.prepareToPlay()
+        audioPlayer.play()
+    }
 }
 
